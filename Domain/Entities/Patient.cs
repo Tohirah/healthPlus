829d2bcf06fee55f4c6d114495b4608a21630244
Tohirah Bankole <tohirahbankole@gmail.com>
2023-01-11 07:55:52 +0100

    Add project files.

---
+++ Domain/Entities/Patient.cs
@@ -0,0 +1,16 @@
+﻿using HealthPlus.Domain.Enums;
+
+namespace HealthPlus.Domain.Entities
+{
+    public class Patient
+    {
+
+        public DateTime DateOfBirth { get; set; }
+        public string BloodGroup { get; set; }
+        public string Genotype { get; set; }
+        public string Allergies { get; set; }
+        public string EmergencyContact { get; set; }
+        public int UserId { get; set; }
+        public User User { get; set; }
+    }
+}
---
+++ Domain/Enums/Gender.cs
@@ -0,0 +1,8 @@
+﻿namespace HealthPlus.Domain.Enum
+{
+    public enum Gender
+    {
+        Male = 1,
+        Female
+    }
+}
