5d1162cf56bda1cc32e9ac6e2c7d4576255b038b
Tohirah Bankole <tohirahbankole@gmail.com>
2023-01-28 22:44:20 +0100

    medic

---
+++ Application/DTOs/CreateMedicalRecordRequestModel.cs
@@ -0,0 +1,13 @@
+﻿using HealthPlus.Domain.Entities;
+
+namespace HealthPlus.Application.DTOs
+{
+    public class CreateMedicalRecordRequestModel
+    {
+        public int Id { get; set; }
+        public int PatientId { get; set; }
+        public Patient Patient { get; set; }
+        public List<Consultation> Consultations { get; set; }
+        public Consultation Consultation { get; set; }
+    }
+}
--- Application/Services/MedicalRecordService.cs
+++ Application/Services/MedicalRecordService.cs
@@ -1,8 +1,24 @@
-﻿using HealthPlus.Application.Interfaces.Services;
+﻿using HealthPlus.Application.DTOs;
+using HealthPlus.Application.Interfaces.Services;
+using HealthPlus.Domain.Entities;
 
 namespace HealthPlus.Application.Services
 {
     public class MedicalRecordService : IMedicalRecordService
     {
+        public BaseResponse CreateMedicalRecord(CreateMedicalRecordRequestModel request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public MedicalRecordResponseModel GetMedicalRecord(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public BaseResponse UpdateMedicalRecord(int id, Consultation consultation)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
